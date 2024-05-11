423062e8b1545dc25a2563e4e9627dc3f07f03b7
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-05-11 09:02:08 -0300

    achorsStyles

--- notes/src/app/tabs/tabs.page.ts
+++ notes/src/app/tabs/tabs.page.ts
@@ -1,4 +1,4 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { Component, EnvironmentInjector, OnInit, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
@@ -10,10 +10,51 @@ import { triangle, ellipse, square } from 'ionicons/icons';
   standalone: true,
   imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
     addIcons({ triangle, ellipse, square });
   }
+
+  ngOnInit(): void {
+    
+    const labelStyle:any = document.getElementById('labelStyle')
+    const labelStyle2:any = document.getElementById('labelStyle2')
+
+     if(window.location.href == "http://localhost:8100/tabs/tab1") {
+
+     labelStyle.style.color = "#2ee6a8"
+     labelStyle2.style.color = "black"
+
+     } 
+    
+     if(window.location.href == "http://localhost:8100/tabs/tab2") {
+
+     labelStyle2.style.color = "#2ee6a8"
+     labelStyle.style.color = "black"
+
+    }
+
+  }
+
+
+  createNote2() {
+    const labelStyle2:any = document.getElementById('labelStyle2')
+    const labelStyle:any = document.getElementById('labelStyle')
+
+    labelStyle2.style.color = "#2ee6a8"
+    labelStyle.style.color = "black"
+
+  }
+
+    allNotes() {
+    const labelStyle2:any = document.getElementById('labelStyle2')
+    const labelStyle:any = document.getElementById('labelStyle')
+
+    labelStyle.style.color = "#2ee6a8"
+    labelStyle2.style.color = "black"
+
+  }
+
 }
